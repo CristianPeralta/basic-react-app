c986ce3120b057b1e2d7018dc381425798b744d0
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-19 12:19:53 -0500

    configure react and reactDOM

--- src/inde.js
+++ src/inde.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+
 import './inde.css';
+import App from './App';
+
+const app = (
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+);
+
+ReactDOM(app, document.getElementById('root'));
