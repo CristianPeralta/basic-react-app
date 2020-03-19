dd265c72818e039501509e4a53d0fc6ca3bcc00e
CristianPeralta <cbperaltasegura@gmail.com>
2020-03-19 12:07:41 -0500

    add links with react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class App extends Component {
     render() {
         return (
-            <div>App</div>
+            <div>
+                <div>
+                    <Link to="/">Users</Link>
+                    <Link to="/pizza">Pizza</Link>
+                </div>
+            </div>
         );
     }
 }
